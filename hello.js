06fe36ffd8bfd2446bb7959ec6b501e54448e209
LusyaPetrosyan <plusine99@gmail.com>
2021-12-15 23:49:07 +0400

    Brief 3

---
+++ hello.js
@@ -0,0 +1,24 @@
+let a = 3;
+
+
+ function sayHi(){
+    console.log("hello Lusya");
+}
+
+ const PI = 3.14;
+ class Dog {
+    woof() {
+        console.log("woof woof");
+    }
+}
+
+
+export {
+    sayHi,
+    PI,
+    Dog
+}
+
+export default function foo() {
+    console.log("fooooo");
+}
---
+++ script.js
@@ -0,0 +1,315 @@
+let programmers = [
+  {id: 1,
+  image: "",
+  firstName: "",
+  lastName: "",
+  email: "",
+  experince: "",
+  education: "",
+  phone: "",
+  },
+  {id: 2,
+  image: "./img/Alex.jpg",
+  firstName: "Alexandr",
+  lastName: "Hambardzumyan",
+  email: "alexanhambarcumyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 97705090",
+},
+  {
+  id: 3,
+  image: "./img/Arus.jpg",
+  firstName: "Arusyak",
+  lastName: "Aghajanyan",
+  email: "arusyakaghajanyan@gmail.com",
+  experience: "Google",
+  education: "",
+  phone: "+374 91900151"
+
+},
+  {
+  id: 4,
+  image: "./img/Lus.jpg",
+  firstName: "Lusya",
+  lastName: "Petrosyan",
+  email: "plusune99@gmail.com",
+  experience: "",
+  education: "Yerevan State University faculty of Radiophysics",
+  phone: "+374 93322902",
+},
+{
+  id: 5,
+  image: "./img/Narek.png",
+  firstName: "Narek",
+  lastName: "Tovmasyan",
+  email: "narek.tovmasyan@gmail.com",
+  experience: "",
+  education: "ASUE",
+  phone: "+374 94276700",
+},
+{
+  id: 6,
+  image: "",
+  firstName: "Suren",
+  lastName: "Galstyan",
+  email: "suren.galstyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 77760986",
+},
+ {
+  id: 7,
+  image: "./img/Hasmik.jpg",
+  firstName: "Hasmik",
+  lastName: "Gevorgyan",
+  email: "hasmik.gevorgyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "374 99463623",
+},
+ {
+  id: 8,
+  image: "",
+  firstName: "Sevak",
+  lastName: "Stepanyan",
+  email: "sevak.stepanyan@gmail.com",
+  experience: "undefined",
+  education: "",
+  phone: "+374 77121696",
+},
+ {
+  id: 9,
+  image: "./img/Larine.jpg",
+  firstName: "Larine",
+  lastName: "Danielyan",
+  email: "lari.danielyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 97759599",
+},
+ {
+  id: 10,
+  image: "./img/Armenak.jpg",
+  firstName: "Armenak",
+  lastName: "Qocharyan",
+  email: "armenak.qocharyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 94902171",
+},
+ {
+  id: 11,
+  image: "./img/Tigran.jpg",
+  firstName: "Tigran",
+  lastName: "Martirosyan",
+  email: "tigran.martirosyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 77121696",
+},
+ {
+  id: 12,
+  image: "./img/Seryoja.jpg",
+  firstName: "Seryoja",
+  lastName: "Asatryan",
+  email: "seryoja.asatryan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 94246880",
+},
+ {
+  id: 13,
+  image: "./img/Marat.jpg",
+  firstName: "Marat",
+  lastName: "Harutyunyan",
+  email: "marat.pharutyunyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 41555152",
+},
+ {
+  id: 14,
+  image: "./img/Lusine Y.jpg",
+  firstName: "Lusine",
+  lastName: "Eremyan",
+  email: "lusine.eremyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 43181834",
+},
+ {
+  id: 15,
+  image: "",
+  firstName: "Babken",
+  lastName: "Tadevosyan",
+  email: "babken.tadevosyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 93285595",
+},
+ {
+  id: 16,
+  image: "",
+  firstName: "Hermine",
+  lastName: "Sahakyan",
+  email: "hermine.sahakyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 97336216",
+},
+ {
+  id: 17,
+  image: "./img/Hermine M.jpg",
+  firstName: "Hermine",
+  lastName: "Melqumyan",
+  email: "hermine.melqumyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 43266599",
+},
+{
+  id: 18,
+  image: "",
+  firstName: "Afina",
+  lastName: "Panyan",
+  email: "afina.panyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 96333328",
+},
+ {
+  id: 19,
+  image: "",
+  firstName: "Liana",
+  lastName: "Dashyan",
+  email: "liana.dashyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 91555374",
+},
+ {
+  id: 20,
+  image: "",
+  firstName: "Anna",
+  lastName: "Hairyan",
+  email: "anna.hairyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 98144450",
+},
+ {
+  id: 21,
+  image: "./img/Anahit.jpg",
+  firstName: "Anahit",
+  lastName: "Khechumyan",
+  email: "anahit.khechumyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 44148000",
+},
+ {
+  id: 22,
+  image: "./img/Ani.jpg",
+  firstName: "Ani",
+  lastName: "Antonyan",
+  email: "ani.antonyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 41900802",
+},
+ {
+  id: 23,
+  image: "./img/Seda.jpg",
+  firstName: "Seda",
+  lastName: "Shahnazaryan",
+  email: "seda.shahnazaryan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 97313871",
+},
+ {
+  id: 24,
+  image: "",
+  firstName: "Silva",
+  lastName: "Ohanyan",
+  email: "silva.ohanyan@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 98200272",
+ },
+{
+ id: 25,
+ image: "./img/Karen.jpg",
+ firstName: "Karen",
+ lastName: "Darbinyan",
+ email: "kar31100@gmail.com",
+ experience: "",
+ education: "",
+ phone: "+374 98156247",
+},
+{
+  id: 26,
+  image: "./img/Narine.jpg",
+  firstName: "Narine",
+  lastName: "Avetisyan",
+  email: "avetisnnarine@gmail.com",
+  experience: "",
+  education: "",
+  phone: "+374 93279190",
+ }
+
+];
+
+console.log(programmers);
+list();
+
+function list() {
+  let container = document.querySelector(".container");
+  programmers.forEach(function (programmer) {
+    container.innerHTML += `<div class="card">      
+        <img src="${programmer.image || "./img/person-icon.jpg"}" alt="person-icon"> 
+        <div class = "bio">
+        <span className="firstName">${programmer.firstName}</span>
+        <span className="lastName">${programmer.lastName}</span>
+         </div>
+          <div class="btn">
+          <a href="#bioPage?=${programmer.id}" class="biolink"><button>Read More</button></a>
+          </div>
+          
+          </div>`;
+  });
+}
+
+window.addEventListener("load", () => {
+  console.log(window.location);
+});
+
+window.addEventListener("load", function () {
+  console.log("LOAD eventlistener called");
+});
+window.addEventListener("hashchange", function () {
+  let container = document.querySelector(".container");
+  let personalPage = document.querySelector(".box");
+  if (window.location.hash.includes("bioPage")) {
+    container.classList.add("hidden");
+    personalPage.classList.remove("hidden");
+    let hash = window.location.hash.split("=");
+    let id = hash[1]; 
+    console.log(id);
+    console.log(programmers[id - 1]);
+    addUserData(programmers[id - 1]);
+  } else {
+    container.classList.remove("hidden");
+    personalPage.classList.add("hidden");
+  }
+});
+function addUserData(programmer) {
+  console.log("personalData===", programmer);
+  document.querySelector(".firstName").innerHTML = programmer.firstName;
+  document.querySelector(".lastName").innerHTML = programmer.lastName;
+  document.querySelector(".email").innerHTML = programmer.email;
+  document.querySelector(".educationName ").innerHTML = programmer.education;
+  document.querySelector(".personImage").setAttribute ("src", programmer.image || "./img/person-icon.jpg");
+  document.querySelector(".phone").innerHTML = programmer.phone || "";
+}
